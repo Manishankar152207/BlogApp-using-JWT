6835866635529772c2f9d7e8a6923240a5cfa778
ucsitservices <ucs.its1@gmail.com>
2023-06-14 10:59:59 +0530

    Done

--- api/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import permissions
-
-class ExcludeAuthenticationForGetMethod(permissions.BasePermission):
-    """Returns the permission based on the type of action"""
-    def has_permission(self, request, view):
-        if request.method == 'GET':
-            return True
-        return False
